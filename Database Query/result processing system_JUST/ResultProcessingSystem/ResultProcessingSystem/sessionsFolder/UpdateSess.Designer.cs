85e3ee19fa829526746d9c6007e5f286846d5549
MD Nazmul Hasan Sheum <mohammadsheam@gmail.com>
2017-07-21 16:16:22 +0600

    Add files via upload

---
+++ Database Query/result processing system_JUST/ResultProcessingSystem/ResultProcessingSystem/sessionsFolder/UpdateSess.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace ResultProcessingSystem.sessionsFolder
+{
+    partial class UpdateSess
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.buttonPDelNew = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBoxSessNew = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBoxPDelNew = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxProgDel = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.comboBoxProgDel);
+            this.panel2.Controls.Add(this.buttonPDelNew);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.comboBoxSessNew);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.comboBoxPDelNew);
+            this.panel2.Font = new System.Drawing.Font("SolaimanLipi", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel2.Location = new System.Drawing.Point(34, 77);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(556, 133);
+            this.panel2.TabIndex = 5;
+            // 
+            // buttonPDelNew
+            // 
+            this.buttonPDelNew.Location = new System.Drawing.Point(319, 85);
+            this.buttonPDelNew.Name = "buttonPDelNew";
+            this.buttonPDelNew.Size = new System.Drawing.Size(75, 23);
+            this.buttonPDelNew.TabIndex = 4;
+            this.buttonPDelNew.Text = "পরিবর্তন ";
+            this.buttonPDelNew.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(23, 88);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 17);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "সেশন বাছাইঃ ";
+            // 
+            // comboBoxSessNew
+            // 
+            this.comboBoxSessNew.FormattingEnabled = true;
+            this.comboBoxSessNew.Location = new System.Drawing.Point(104, 85);
+            this.comboBoxSessNew.Name = "comboBoxSessNew";
+            this.comboBoxSessNew.Size = new System.Drawing.Size(186, 25);
+            this.comboBoxSessNew.TabIndex = 2;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(21, 18);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 17);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "বিভাগ বাছাইঃ ";
+            // 
+            // comboBoxPDelNew
+            // 
+            this.comboBoxPDelNew.FormattingEnabled = true;
+            this.comboBoxPDelNew.Location = new System.Drawing.Point(104, 18);
+            this.comboBoxPDelNew.Name = "comboBoxPDelNew";
+            this.comboBoxPDelNew.Size = new System.Drawing.Size(396, 25);
+            this.comboBoxPDelNew.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("SolaimanLipi", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(80, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(473, 34);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "নতুন সেশন প্রবেশে ভুল করলে পরিবর্তন করুন ";
+            // 
+            // comboBoxProgDel
+            // 
+            this.comboBoxProgDel.FormattingEnabled = true;
+            this.comboBoxProgDel.Location = new System.Drawing.Point(104, 50);
+            this.comboBoxProgDel.Name = "comboBoxProgDel";
+            this.comboBoxProgDel.Size = new System.Drawing.Size(186, 25);
+            this.comboBoxProgDel.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "প্রোগ্রাম বাছাইঃ ";
+            // 
+            // UpdateSess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 240);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Name = "UpdateSess";
+            this.Text = "UpdateSess";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button buttonPDelNew;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBoxSessNew;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox comboBoxPDelNew;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxProgDel;
+
+    }
+}
